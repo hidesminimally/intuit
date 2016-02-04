42d92aaa9435af37d2703a12001fd38daa16c837
Daniel Li <danielmxli@gmail.com>
2016-02-04 01:32:41 -0800

    Remove comments

--- get_path.js
+++ get_path.js
@@ -1,27 +1,3 @@
-/**
-Get full path on an element from the JSON below.  For example the full path of item2 would be /itemList/items[1]/id.
-
-{"itemList": {"items": [
-    {"id": "item1"},
-    {"id": "item2","label": "Item 2"},
-    {"id": "item3"},
-    {"id": "item4"},
-    {"id": "item5"},
-    {"id": "subItem1",
-        "subItems": [
-            {"id": "subItem1Item1","label": "SubItem 1"},
-            {"id": "subItem1Item2","label": "SubItem 2"},
-            {"id": "subItem1Item3","label": "SubItem 3"}
-        ]
-    },
-    {"id": "item6"},
-    {"id": "item7","label": "Item 7"},
-    {"id": "subItem2",
-        "subItems": {"id": "item1","label": "SubItem 2 item1"}
-    }
-]}}
-**/
-
 function getJsonPath(data, element, path) {
   var fullPath = [];
   path = path || "";
@@ -43,10 +19,7 @@ function getJsonPath(data, element, path) {
   }
   return fullPath.toString();
 }
-
-
-
-var items =
+var json =
 {"itemList": {"items": [
     {"id": "item1"},
     {"id": "item2","label": "Item 2"},
@@ -67,5 +40,5 @@ var items =
     }
 ]}}
 
-var test = getJsonPath(items, "item2");
+var test = getJsonPath(json, "item2");
 console.log(test)
